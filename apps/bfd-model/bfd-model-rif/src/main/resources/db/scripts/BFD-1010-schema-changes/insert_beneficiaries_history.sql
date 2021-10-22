6cfb125ccf50b47734436c41f1733c22c4a5866b
mackec1 <chris.mackenzie@adhocteam.us>
2021-10-22 13:10:55 -0400

    add bfd1010 changes into this branch; bfd1010 will never be merged

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/insert_beneficiaries_history.sql
@@ -0,0 +1,27 @@
+insert into public.beneficiaries_history (
+	bene_history_id,
+    bene_id,
+    bene_birth_dt,
+    bene_sex_ident_cd,
+    bene_crnt_hic_num,
+    mbi_num,
+    hicn_unhashed,
+    mbi_hash,
+    efctv_bgn_dt,
+    last_updated)
+select
+	"beneficiaryHistoryId", 
+	Cast("beneficiaryId" as bigint),
+	"birthDate",
+	"sex", 
+	"hicn", 
+	"medicareBeneficiaryId", 
+	"hicnUnhashed", 
+	"mbiHash", 
+	"mbiEffectiveDate", 
+	"lastupdated"
+from
+	public."BeneficiariesHistory"
+on conflict on constraint
+	beneficiaries_history_pkey
+do nothing;
---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/insert_beneficiary_monthly.sql
@@ -0,0 +1,39 @@
+insert into public.beneficiary_monthly 
+( 
+	year_month,
+	partd_contract_number_id,
+	bene_id,
+	partc_contract_number_id,
+	fips_state_cnty_code,
+	medicare_status_code,
+	entitlement_buy_in_ind,
+	hmo_indicator_ind,
+	partc_pbp_number_id,
+	partc_plan_type_code,
+	partd_pbp_number_id,
+	partd_segment_number_id,
+	partd_retiree_drug_subsidy_ind,
+	partd_low_income_cost_share_group_code,
+	medicaid_dual_eligibility_code
+) 
+select 
+	"yearMonth",
+	"partDContractNumberId",
+	cast("parentBeneficiary" as bigint),
+	"partCContractNumberId",
+	"fipsStateCntyCode",
+	"medicareStatusCode",
+	"entitlementBuyInInd",
+	"hmoIndicatorInd",
+	"partCPbpNumberId",
+	"partCPlanTypeCode",
+	"partDPbpNumberId",
+	"partDSegmentNumberId",
+	"partDRetireeDrugSubsidyInd",
+	"partDLowIncomeCostShareGroupCode",
+	"medicaidDualEligibilityCode"
+from
+	public."BeneficiaryMonthly"
+on conflict on constraint
+	beneficiary_monthly_pkey
+do nothing;
