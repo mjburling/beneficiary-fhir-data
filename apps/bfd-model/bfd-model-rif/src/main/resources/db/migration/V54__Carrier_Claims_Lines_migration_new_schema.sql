aa3bbe6238ee9ea24e08149de5ff12e4343e9507
mackec1 <chris.mackenzie@adhocteam.us>
2022-01-31 15:30:00 -0500

    migrate carrier_claim_lines to gauge flyway migration duration

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V54__Carrier_Claims_Lines_migration_new_schema.sql
@@ -0,0 +1,158 @@
+create table if not exists carrier_claim_lines_bfd1485 (
+    clm_id                                   bigint not null,                          -- parentClaim
+    line_num                                 smallint not null,                        -- lineNumber
+    line_nch_pmt_amt                         numeric(10,2) not null,                   -- paymentAmount
+    line_1st_expns_dt                        date,                                     -- firstExpenseDate
+    line_alowd_chrg_amt                      numeric(10,2) not null,                   -- allowedChargeAmount
+    line_bene_pmt_amt                        numeric(10,2) not null,                   -- beneficiaryPaymentAmount
+    line_bene_prmry_pyr_cd                   character(1),                             -- primaryPayerCode
+    line_bene_prmry_pyr_pd_amt               numeric(10,2) not null,                   -- primaryPayerPaidAmount
+    line_bene_ptb_ddctbl_amt                 numeric(10,2) not null,                   -- beneficiaryPartBDeductAmount
+    line_cms_type_srvc_cd                    character(1) not null,                    -- cmsServiceTypeCode
+    line_coinsrnc_amt                        numeric(10,2) not null,                   -- coinsuranceAmount
+    line_hct_hgb_rslt_num                    numeric(4,1) not null,                    -- hctHgbTestResult
+    line_hct_hgb_type_cd                     character varying(2),                     -- hctHgbTestTypeCode
+    line_icd_dgns_cd                         character varying(7),                     -- diagnosisCode
+    line_icd_dgns_vrsn_cd                    character(1),                             -- diagnosisCodeVersion
+    line_last_expns_dt                       date,                                     -- lastExpenseDate
+    line_ndc_cd                              character varying(11),                    -- nationalDrugCode
+    line_place_of_srvc_cd                    character varying(2) not null,            -- placeOfServiceCode
+    line_pmt_80_100_cd                       character(1),                             -- paymentCode
+    line_prvdr_pmt_amt                       numeric(10,2) not null,                   -- providerPaymentAmount
+    line_prcsg_ind_cd                        character varying(2),                     -- processingIndicatorCode
+    line_sbmtd_chrg_amt                      numeric(10,2) not null,                   -- submittedChargeAmount
+    line_service_deductible                  character(1),                             -- serviceDeductibleCode
+    line_srvc_cnt                            smallint not null,                        -- serviceCount
+    carr_line_mtus_cd                        character(1),                             -- mtusCode
+    carr_line_mtus_cnt                       integer not null,                         -- mtusCount
+    betos_cd                                 character varying(3),                     -- betosCode
+    carr_line_ansthsa_unit_cnt               smallint not null,                        -- anesthesiaUnitCount
+    carr_line_clia_lab_num                   character varying(10),                    -- cliaLabNumber
+    carr_line_prcng_lclty_cd                 character varying(2) not null,            -- linePricingLocalityCode
+    carr_line_prvdr_type_cd                  character(1) not null,                    -- providerTypeCode
+    carr_line_rdcd_pmt_phys_astn_c           character(1) not null,                    -- reducedPaymentPhysicianAsstCode
+    carr_line_rx_num                         character varying(30),                    -- rxNumber
+    carr_prfrng_pin_num                      character varying(15) not null,           -- performingProviderIdNumber
+    hcpcs_1st_mdfr_cd                        character varying(5),                     -- hcpcsInitialModifierCode
+    hcpcs_2nd_mdfr_cd                        character varying(5),                     -- hcpcsSecondModifierCode
+    hcpcs_cd                                 character varying(5),                     -- hcpcsCode
+    hpsa_scrcty_ind_cd                       character(1),                             -- hpsaScarcityCode
+    org_npi_num                              character varying(10),                    -- organizationNpi
+    prf_physn_npi                            character varying(12),                    -- performingPhysicianNpi
+    prf_physn_upin                           character varying(12),                    -- performingPhysicianUpin
+    prtcptng_ind_cd                          character(1),                             -- providerParticipatingIndCode
+    prvdr_spclty                             character varying(3),                     -- providerSpecialityCode
+    prvdr_state_cd                           character varying(2),                     -- providerStateCode
+    prvdr_zip                                character varying(9),                     -- providerZipCode
+    tax_num                                  character varying(10) not null,           -- providerTaxNumber  
+
+    constraint carrier_claim_lines_bfd1485_pkey
+        primary key (clm_id, line_num),
+
+    constraint carrier_claim_lines_clm_id_to_carrier_claims_bfd1485
+        foreign key (clm_id) references carrier_claims_bfd1485(clm_id)
+);
+
+${logic.psql-only} SET max_parallel_workers = 6;
+${logic.psql-only} SET max_parallel_workers_per_gather = 6;
+${logic.psql-only} SET parallel_leader_participation = off;
+${logic.psql-only} SET parallel_tuple_cost = 0;
+${logic.psql-only} SET parallel_setup_cost = 0;
+${logic.psql-only} SET min_parallel_table_scan_size = 0;
+
+insert into carrier_claim_lines_bfd1485(
+	clm_id ,
+	line_num,
+	line_nch_pmt_amt,
+	line_1st_expns_dt,
+	line_alowd_chrg_amt,
+	line_bene_pmt_amt,
+	line_bene_prmry_pyr_cd,
+	line_bene_prmry_pyr_pd_amt,
+	line_bene_ptb_ddctbl_amt,
+	line_cms_type_srvc_cd,
+	line_coinsrnc_amt,
+	line_hct_hgb_rslt_num,
+	line_hct_hgb_type_cd,
+	line_icd_dgns_cd,
+	line_icd_dgns_vrsn_cd,
+	line_last_expns_dt,
+	line_ndc_cd,
+	line_place_of_srvc_cd,
+	line_pmt_80_100_cd,
+	line_prvdr_pmt_amt,
+	line_prcsg_ind_cd,
+	line_sbmtd_chrg_amt,
+	line_service_deductible,
+	line_srvc_cnt,
+	carr_line_mtus_cd,
+	carr_line_mtus_cnt,
+	betos_cd,
+	carr_line_ansthsa_unit_cnt,
+	carr_line_clia_lab_num,
+	carr_line_prcng_lclty_cd,
+	carr_line_prvdr_type_cd,
+	carr_line_rdcd_pmt_phys_astn_c,
+	carr_line_rx_num,
+	carr_prfrng_pin_num,
+	hcpcs_1st_mdfr_cd,
+	hcpcs_2nd_mdfr_cd,
+	hcpcs_cd,
+	hpsa_scrcty_ind_cd,
+	org_npi_num,
+	prf_physn_npi,
+	prf_physn_upin,
+	prtcptng_ind_cd,
+	prvdr_spclty,
+	prvdr_state_cd,
+	prvdr_zip,
+	tax_num
+)
+select
+	cast(clm_id as bigint),
+	line_num,
+	line_nch_pmt_amt,
+	line_1st_expns_dt,
+	line_alowd_chrg_amt,
+	line_bene_pmt_amt,
+	line_bene_prmry_pyr_cd,
+	line_bene_prmry_pyr_pd_amt,
+	line_bene_ptb_ddctbl_amt,
+	line_cms_type_srvc_cd,
+	line_coinsrnc_amt,
+	line_hct_hgb_rslt_num,
+	line_hct_hgb_type_cd,
+	line_icd_dgns_cd,
+	line_icd_dgns_vrsn_cd,
+	line_last_expns_dt,
+	line_ndc_cd,
+	line_place_of_srvc_cd,
+	line_pmt_80_100_cd,
+	line_prvdr_pmt_amt,
+	line_prcsg_ind_cd,
+	line_sbmtd_chrg_amt,
+	line_service_deductible,
+	line_srvc_cnt,
+	carr_line_mtus_cd,
+	carr_line_mtus_cnt,
+	betos_cd,
+	carr_line_ansthsa_unit_cnt,
+	carr_line_clia_lab_num,
+	carr_line_prcng_lclty_cd,
+	carr_line_prvdr_type_cd,
+	carr_line_rdcd_pmt_phys_astn_c,
+	carr_line_rx_num,
+	carr_prfrng_pin_num,
+	hcpcs_1st_mdfr_cd,
+	hcpcs_2nd_mdfr_cd,
+	hcpcs_cd,
+	hpsa_scrcty_ind_cd,
+	org_npi_num,
+	prf_physn_npi,
+	prf_physn_upin,
+	prtcptng_ind_cd,
+	prvdr_spclty,
+	prvdr_state_cd,
+	prvdr_zip,
+	tax_num
+from carrier_claim_lines;
